aff0d773caf8b45ad882b513f512e4b35a64fe71
tucno21 <carlitostucno@gmail.com>
2021-02-23 22:13:02 -0500

    agregado ventapagina

--- dev/js/main-menu.js
+++ dev/js/main-menu.js
@@ -17,3 +17,17 @@ var counter = 1;
         }
     }, 5000);
 
+
+let thumbnails = document.getElementsByClassName('ventas-slider__thumbnail')
+let activeImages = document.getElementsByClassName('ventas-slider__active')
+
+for (var i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener('mouseover', function () {
+        console.log(activeImages)
+        if (activeImages.length > 0) {
+            activeImages[0].classList.remove('ventas-slider__active')
+        }
+        this.classList.add('ventas-slider__active')
+        document.getElementById('visor-img').src = this.src
+    })
+}
